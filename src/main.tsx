9b3a2810fd88df702273e5ba90492bb382d5e0bd
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2026-05-22 11:42:18 +0700

    rect strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,5 @@ const router = getRouter();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
