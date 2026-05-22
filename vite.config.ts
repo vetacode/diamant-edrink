086fe381023d0315bc2522d1f1340c3c68fa1a6c
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2026-05-22 11:41:25 +0700

    adds index html and chang vite config accordingly

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -9,5 +11,13 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    middlewareMode: false,
   },
 });
